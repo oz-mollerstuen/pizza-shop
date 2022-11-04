3eec7281adb4d54033a7169916fc21e4c6b04346
Lucas Mollerstuen <mollerstuen.works@gmail.com>
2022-11-04 16:50:19 -0700

    WIP: having issues connecting constructors

--- js/scripts.js
+++ js/scripts.js
@@ -3,63 +3,62 @@
   function Pies(size, addOns, sauce) {
     this.size = size;
     this.addOns = addOns;
-    this.sauce = sauce;
-  }
+  };
+  let tempPrice2 = 0;
+  let tempPrice1 = 0;
 
 Pies.prototype.sizePrices = function() {
-  let tempPrice1 = 0;
+  
     if(this.size === "lg") {
-      this.tempPrice += 15;
+      this.tempPrice1 += 15;
+      console.log("made it here");
     }  
       else if(this.size === "med") {
-        this.tempPrice += 12;
+        this.tempPrice1 += 12;
       }
       else if(this.size === "sm") {
-        this.tempPrice += 10;
+        this.tempPrice1 += 10;
       }
       return tempPrice1;
+      console.log(tempPrice1)
 }  
   Pies.prototype.addOnPrices = function() {
-    let tempPrice2 = 0;
+    
 
     if(this.addOns.includes("Sausage")) {
-      this.tempPrice += 2;
+      this.tempPrice2 += 2;
     }  
       else if(this.addOns.includes("Mushrooms")) {
-        this.tempPrice += 2;
+        this.tempPrice2 += 2;
       }
       else if(this.addOns.includes("Pepperoni")) {
-        this.tempPrice += 2;
+        this.tempPrice2 += 2;
       }
       else {
-        this.tempPrice += 0;
+        this.tempPrice2 += 0;
       }
       return tempPrice2
   }
-
-  Pies.prototype.addTop = function(addOns) {
-    this.addOns.push(addOns);
-}
-
-
-
+  
 //--------------------UI Logic--------------------//
-let Pie = new Pies();
-
- 
-        //finalCost.addOnPrices() 
-        
 
 function handleSubmit(event) {
   event.preventDefault();
+  
   const sizeChoice = document.getElementById("size").value
   const saus1 = document.getElementById("sausage").checked
   const mush1 = document.getElementById("mushrooms").checked
   const pep1 = document.getElementById("pepperoni").checked
 
-let finalCost = (tempPrice1 + tempPrice2)
-document.getElementById("cost").innerHTML = finalCost;
-}
+  const newPies = new Pies(sizeChoice, saus1, mush1, pep1);
+  const newPies2= new Pies(sizeChoice, saus1, mush1, pep1);
+  const finalPie = newPies.sizePrices().value;
+  const finalPie2 = newPies2.addOnPrices().value;
+
+  const finalCost = (finalPie + finalPie2)
+  document.getElementById("cost").innerHTML = toString(finalCost);
+
+};
 
 window.addEventListener("load", function() {
   this.document.getElementById("pie-choices").addEventListener("submit", handleSubmit);
